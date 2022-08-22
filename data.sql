b1f54ab3c4c693902b8d36f053b0c3d9f635cf4e
Desor Guishny <desorguishny@gmail.com>
2022-08-22 15:54:08 -0400

    final changes

--- data.sql
+++ data.sql
@@ -80,6 +80,7 @@ animal_ids,
 vets_ids, generate_series('1980-01-01'::timestamp, '2021-01-01', '4 hours')
 visit_timestamp;
 
-insert into owners (full_name, email) select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
+insert into owners (full_name, email)
+select 'Owner ' || generate_series(1,2500000), 'owner_' || generate_series(1,2500000) || '@mail.com';
 
 SELECT * FROM owners;
