e94720be8d1a335be39480804b5ae511949fe2d6
judah <87juda@gmail.com>
2022-08-22 14:43:24 -0300

    Test commit from Judah

--- schema.sql
+++ schema.sql
@@ -1,4 +1,5 @@
 /* Database schema to keep the structure of entire database. */
+-- judah is on
 
 create table animals(id integer, name text, date_of_birth date, escape_attempts integer, neutered boolean, weight_kg decimal);
 
